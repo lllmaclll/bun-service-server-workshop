1a6df048f1ec2f606c047ae3c07c6ca2c6f2e882
lllmaclll <makky1593@gmail.com>
2024-12-03 21:40:55 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from "elysia";
+import jwt from "@elysiajs/jwt";
+import cors from "@elysiajs/cors";
+import { UserController } from "./controllers/UserController";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.use(cors())
+.use(jwt({
+  name: "jwt",
+  secret: "secret"
+}))
+  .get("/", () => "Hello Elysia")
+  .post("/api/user/sign-in", UserController.signIn)
+  .listen(3002);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`

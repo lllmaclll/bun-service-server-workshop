5faa1ee4fa2f3c9f4e29d458c8143969320182c2
lllmaclll <makky1593@gmail.com>
2024-12-12 15:26:16 +0700

    day 4

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { password } from "bun";
 
 const prisma = new PrismaClient()
 
@@ -58,6 +59,82 @@ export const UserController = {
 
             await prisma.user.update({
                 where: { id },
+                data: newData
+            })
+
+            return { message: "success" }
+        } catch (error) {
+            return error
+        }
+    },
+    list: async () => {
+        try {
+            const users = await prisma.user.findMany({
+                select: {
+                    id: true,
+                    username: true,
+                    level: true
+                },
+                where: {
+                    status: 'active'
+                },
+                orderBy: {
+                    id: 'desc'
+                }
+            })
+
+            return users
+        } catch (error) {
+            return error
+        }
+    },
+    createUser: async ({ body }: {
+        body: {
+            username: string,
+            password: string,
+            level: string;
+        }
+    }) => {
+        try {
+            await prisma.user.create({
+                data: body
+            })
+
+            return { message: "success" }
+        } catch (error) {
+            return error
+        }
+    },
+    updateUser: async ({ body, params }: {
+        body: {
+            username: string;
+            password: string;
+            level: string;
+        },
+        params: {
+            id: string
+        }
+    }) => {
+        try {
+            const oldUser = await prisma.user.findUnique({
+                select: {
+                    password: true
+                },
+                where: {
+                    id: parseInt(params.id)
+                }
+            })
+
+            const newData = {
+                username: body.username,
+                password: body.password ?? oldUser?.password,
+                level: body.level
+            }
+
+            await prisma.user.update({
+                where: {
+                    id: parseInt(params.id)
+                },
                 data: newData
             })
 
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Elysia } from "elysia";
 import jwt from "@elysiajs/jwt";
 import cors from "@elysiajs/cors";
 import { UserController } from "./controllers/UserController";
+import { DeviceController } from "./controllers/DeviceController";
 
 const app = new Elysia()
 .use(cors())
@@ -10,8 +11,22 @@ const app = new Elysia()
   secret: "secret"
 }))
   .get("/", () => "Hello Elysia")
+
+  // user
   .post("/api/user/sign-in", UserController.signIn)
   .put("/api/user/update", UserController.update)
+  .get("/api/user/list", UserController.list)
+  .post("/api/user/create-user", UserController.createUser)
+  .put("/api/user/update-user/:id", UserController.updateUser)
+
+  // device 
+  .post("/api/device/create", DeviceController.create)
+  .get("/api/device/list", DeviceController.list)
+  .put("/api/device/update/:id", DeviceController.update)
+  .delete("/api/device/remove/:id", DeviceController.remove)
+
+  // listen
+
   .listen(3002);
 
 console.log(
